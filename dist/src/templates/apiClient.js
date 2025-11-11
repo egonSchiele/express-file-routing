3642044c38cc8bdf7c78cee4a68f2bdf6eb40dbf
Aditya Bhargava <adit@adit.io>
2025-11-11 15:16:46 -0600

    dont escape anything

--- dist/src/templates/apiClient.js
+++ dist/src/templates/apiClient.js
@@ -2,22 +2,22 @@
 // Source: /Users/adit/express-file-routing/src/templates/apiClient.mustache
 // Any manual changes will be lost.
 import { apply } from "typestache";
-export const template = `export async function {{functionName:string}}({{{args:string}}}): Promise<{{responseType:string}}> {
+export const template = `export async function {{{functionName:string}}}({{{args:string}}}): Promise<{{{responseType:string}}}> {
     try {
-        const response = await fetch(\`{{url:string}}\`, {
-            method: "{{method:string}}",
+        const response = await fetch(\`{{{url:string}}}\`, {
+            method: "{{{method:string}}}",
             headers: {
               "Content-Type": "application/json",
             },
             ...options,
         });
         if (!response.ok) {
-            console.log(\`Error calling API {{functionName}}\`);
+            console.log(\`Error calling API {{{functionName}}}\`);
         }
         return response.json();
     } catch (error) {
-        console.error(\`Error in {{functionName}}:\`, error);
-        return { success: false, error } as {{responseType:string}};
+        console.error(\`Error in {{{functionName}}}:\`, error);
+        return { success: false, error } as {{{responseType:string}}};
     }
 }
 `;
--- src/templates/apiClient.ts
+++ src/templates/apiClient.ts
@@ -3,22 +3,22 @@
 // Any manual changes will be lost.
 import { apply } from "typestache";
 
-export const template = `export async function {{functionName:string}}({{{args:string}}}): Promise<{{responseType:string}}> {
+export const template = `export async function {{{functionName:string}}}({{{args:string}}}): Promise<{{{responseType:string}}}> {
     try {
-        const response = await fetch(\`{{url:string}}\`, {
-            method: "{{method:string}}",
+        const response = await fetch(\`{{{url:string}}}\`, {
+            method: "{{{method:string}}}",
             headers: {
               "Content-Type": "application/json",
             },
             ...options,
         });
         if (!response.ok) {
-            console.log(\`Error calling API {{functionName}}\`);
+            console.log(\`Error calling API {{{functionName}}}\`);
         }
         return response.json();
     } catch (error) {
-        console.error(\`Error in {{functionName}}:\`, error);
-        return { success: false, error } as {{responseType:string}};
+        console.error(\`Error in {{{functionName}}}:\`, error);
+        return { success: false, error } as {{{responseType:string}}};
     }
 }
 `;
